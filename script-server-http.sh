134387d2d60eab1813cb206eddbe87bdf39a4d96
Hikari <silentyamato10@gmail.com>
2025-10-02 08:56:18 +0700

    Fitur Super Admin' '

--- script-server-http.sh
+++ script-server-http.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-
-python3 -m http.server -d . -b 192.168.100.28 80
+service apache2 stop
+python3 -m http.server -d . -b 127.0.0.1 80
+service apache2 start
--- script-server-php.sh
+++ script-server-php.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 cd system/script-php/
-php -S 192.168.100.28:8000 -d system/script-php/
+php -S 127.0.0.1:8000 -d system/script-php/
 cd ../../
