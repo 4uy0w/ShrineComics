f09e69dc8b66cc082831147206c5efc86da2165f
Hikari <silentyamato10@gmail.com>
2025-10-16 20:20:38 +0700

    update writer

--- system/script-php/writer/create.php
+++ system/script-php/writer/create.php
@@ -13,41 +13,42 @@
 	</head>
 	<body>
 		<div class="add-new-comic-area">
-			<div class="add-new-comic-box">
-				<form action="action-create.php?debug=debug&id_writer=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
-					<section class="section-add-new-comic">
-						<input type="text" name="title" id="input-comic" placeholder="comic title"><br>
-						<input type="number" name="page" id="input-comic" placeholder="comic page"><br>
-						<input type="number" name="price" id="input-comic" placeholder="comic price"><br>
-						<input type="text" name="writer" id="input-comic" placeholder="comic writer"><br>
-						<select name="genre" id="select-genre">
-							<option value="romance">romance</option>
+			<form action="action-create.php?debug=debug&id_writer=<?php echo $id; ?>" method="get" enctype="multipart/form-data">
+				<div class="add-new-comic-box">
+					<section class="add-new-comic-box-metadata">
+						<input type="text" name="title" id="input-comic"><br>
+						<input type="number" name="page" id="input-comic"><br>
+						<input type="number" name="price" id="input-comic"><br>
+						<input type="text" name="writer" id="input-comic"><br>
+						<input type="date" name="release_date" id="input-release-date"><br>
+						<select name="genre" id="input-comic">
 							<option value="comedy">comedy</option>
+							<option value="romance">romance</option>
 							<option value="adventure">adventure</option>
 							<option value="sci-fi">sci-fi</option>
 						</select><br>
-						<input type="date" name="release_date" id="input-comic"><br>
-						<textarea name="comment" id="textarea-comment">
+						<textarea name="comment" id="input-comment">
 
 						</textarea><br>
-						<section class="box-btn">
-							<input type="file" name="image" id="comic-hidden-image">
-							<input type="button" value="Upload Comic" id="btn-upload-comic">
-							<input type="file" name="banner" id="comic-hidden-banner">
-							<input type="button" value="Upload Banner" id="btn-upload-banner"><br>
-							<section class="box-preview-banner">
-								<img src="#no-preview" alt="preview banner" id="image-banner-preview">
-							</section>
-							<section class="image-comic-desc">
-								<img src="#no-image" alt="comic image" id="image-comic-preview">
-							</section>
-						</section>
-						<button type="submit" id="btn-submit">add new</button>
-						<button type="reset" id="btn-reset">reset</button>
-						<a href="index.php?id=<?php echo $id; ?>"><button>back</button></a>
 					</section>
-				</form>
-			</div>
+					<section class="add-new-comic-box-source">
+						<input type="file" name="image" id="hidden-gem">
+						<button type="button" id="image-comic-btn">find comic</button>
+						<input type="file" name="banner" id="hidden-gem">
+						<button type="button" id="banner-comic-btn">find banner</button>
+
+						<section class="image-viewer-section">
+							<img src="#no-image-comic" id="comic-image-view">
+							<img src="#no-image-banner" id="comic-banner-view">
+						</section>
+					</section>
+				</div>
+				<section class="upload-btn-comic-box">
+					<button type="submit">add new comic</button>
+					<button type="reset">reset</button>
+					<a href="index.php?id=<?php echo $id; ?>"></a>
+				</section>
+			</form>
 		</div>
 		<script src="script.js"></script>
 	</body>
